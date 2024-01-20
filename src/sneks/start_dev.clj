79cd685662f3eb9d17a83f8e5c968ee38b791b99
CarnunMP <admin@carnun.com>
2024-01-20 14:35:36 +0000

    Intitial: play-cljc

---
+++ src/sneks/start_dev.clj
@@ -0,0 +1,18 @@
+(ns sneks.start-dev
+  (:require [sneks.start :as start]
+            [sneks.core :as c]
+            [clojure.spec.test.alpha :as st]
+            [clojure.spec.alpha :as s]
+            [play-cljc.gl.core :as pc])
+  (:import [org.lwjgl.glfw GLFW]
+           [sneks.start Window]))
+
+(defn start []
+  (st/instrument)
+  (let [window (start/->window)
+        game (pc/->game (:handle window))]
+    (start/start game window)))
+
+(defn -main []
+  (start))
+
---
+++ src/sneks/start_dev.cljs
@@ -0,0 +1,6 @@
+(ns sneks.start-dev
+  (:require [sneks.start]
+            [clojure.spec.test.alpha :as st]))
+
+(st/instrument)
+
